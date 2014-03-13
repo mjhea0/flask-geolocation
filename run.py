05ceac075e8d753c004155d6b98ac66fcdead0df
Michael Herman <hermanmu@gmail.com>
2014-03-13 11:32:03 -0600

    initial

--- run.py
+++ run.py
@@ -10,5 +10,20 @@ def root():
     geo_data = geolocate.record_by_addr(request.remote_addr)
     return jsonify(geo_data)
 
+@app.route('/ip/<ip_address>')
+def ip(ip_address):  
+    geo_data = geolocate.record_by_addr(ip_address)
+    return jsonify(geo_data)
+
+@app.route('/domain/<domain_name>')
+def domain(domain_name):  
+    geo_data = geolocate.record_by_name(domain_name)
+    return jsonify(geo_data)
+
+@app.errorhandler(500)
+def error_500(e):  
+    return jsonify({'error': 'Error finding GeoIP data for that address'})
+
+
 if __name__ == '__main__':  
     app.run(debug=False)
