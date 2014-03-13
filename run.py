0b05965b9ce68663bc4334ec606bbacd4136ef9d
Michael Herman <hermanmu@gmail.com>
2014-03-13 13:28:43 -0600

    initial

--- run.py
+++ run.py
@@ -1,29 +1,28 @@
-from flask import Flask, request, jsonify  
+from flask import Flask, request, jsonify, render_template
 import pygeoip
 
 app = Flask(__name__)
 
-geolocate = pygeoip.GeoIP('GeoLiteCityv6.dat')
+geolocate = pygeoip.GeoIP('GeoIPCity.dat')
 
 @app.route('/')
-def root():  
-    geo_data = geolocate.record_by_addr(request.remote_addr)
-    return jsonify(geo_data)
+def index():  
+    return render_template("index.html")
 
-@app.route('/ip/<ip_address>')
+@app.route('/api/ip/<ip_address>')
 def ip(ip_address):  
     geo_data = geolocate.record_by_addr(ip_address)
     return jsonify(geo_data)
 
-@app.route('/domain/<domain_name>')
+@app.route('/api/domain/<domain_name>')
 def domain(domain_name):  
     geo_data = geolocate.record_by_name(domain_name)
     return jsonify(geo_data)
 
 @app.errorhandler(500)
 def error_500(e):  
-    return jsonify({'error': 'Error finding GeoIP data for that address'})
+    return jsonify({'error': 'Error finding location data for that address'})
 
 
 if __name__ == '__main__':  
-    app.run(debug=False)
+    app.run(debug=True)
